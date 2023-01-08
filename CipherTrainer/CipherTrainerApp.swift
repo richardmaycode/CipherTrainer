57eb0970519c98f9b3fb73f9a410c392b10fc449
Richard Wise <richard.wise@hey.com>
2023-01-08 15:33:28 -0500

    Initial Commit

---
+++ CipherTrainer/CipherTrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CipherTrainerApp.swift
+//  CipherTrainer
+//
+//  Created by Richard Wise on 1/8/23.
+//
+
+import SwiftUI
+
+@main
+struct CipherTrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
