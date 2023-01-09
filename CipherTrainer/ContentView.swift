cb5ead231cd5db978d796e12ec37c98ae40ce1a5
Richard Wise <richard.wise@hey.com>
2023-01-08 21:41:54 -0500

    Add animation to the shapes

--- CipherTrainer/ContentView.swift
+++ CipherTrainer/ContentView.swift
@@ -10,17 +10,21 @@ import SwiftUI
 struct ContentView: View {
     
     @State var number = 1
+    @State var trimAmount: CGFloat = .zero
+    @State var redrawing: Bool = false
     
     var body: some View {
         VStack(spacing: 20) {
             VStack {
                 CipherShape(number: number)
-                    .stroke(.black, style: StrokeStyle(lineWidth: 15, lineCap: .round, lineJoin: .round))
+                    .trim(from: .zero, to: trimAmount)
+                    .stroke(redrawing ? .clear : .black, style: StrokeStyle(lineWidth: 15, lineCap: .round, lineJoin: .round))
                     .frame(width: 200, height: 300, alignment: .center)
                     .padding(20)
                 
                 Text("\(number)")
                     .font(.largeTitle)
+                    .foregroundColor(.black)
             }
             .padding(20)
             .background {
@@ -29,11 +33,20 @@ struct ContentView: View {
                     .shadow(radius: 4)
             }
             Button {
+                
+                redrawing = true
+                trimAmount = .zero
                 if number != 9 {
                     number += 1
                 } else {
                     number = 1
                 }
+                redrawing = false
+                withAnimation(.linear(duration: 0.75)) {
+                    trimAmount = 1.0
+                }
+                
+                
             } label: {
                 Text("Next")
                     .font(.title)
@@ -42,6 +55,12 @@ struct ContentView: View {
             }
             .buttonStyle(.borderedProminent)
             .padding(.horizontal)
+        }
+        .onAppear {
+            withAnimation(.linear(duration: 0.75)) {
+                trimAmount = 1.0
+            }
+            
         }
     }
 }
